59e8e7be565609c3da0008ecc6c021bb83d87fc3
Lanfranco Nastasi <lanfranco.nastasi@metaltronica.com>
2023-03-07 10:16:41 +0100

    Ripristinato il progetto dello schematico proprietario.

--- scripts/part-list.cmd
+++ scripts/part-list.cmd
@@ -2,7 +2,7 @@
 
 set BOM_EXTENDER="D:\Work\MT\_libs_\orcad-capture-library\scripts\bom-extender.cmd"
 
-cd /d %~dp0..\orcad
+cd /d %~dp0..\capture
 
 call %BOM_EXTENDER% 22-302-0.bom 22-302-0.xlsx
 
